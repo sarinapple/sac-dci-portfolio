3e7ae524f9e0631d2ef3e5708c6e66cd9b6a9ca7
sarina <sacrivello@gmail.com>
2023-06-01 12:17:43 +0200

    initiated scroll progress bar

---
+++ src/portfolio.js
@@ -0,0 +1,49 @@
+const mas = document.querySelector(".row1 li:nth-of-type(2)")
+console.log('here');
+
+console.log(mas);
+
+
+
+const curtain = (e) => {
+
+  console.log('script here');
+
+  const masUl = document.querySelector(".mas")
+  if (masUl.style.display === "" || masUl.style.display === "block") {
+    masUl.style.display = "none"
+  } else {
+    masUl.style.display = "block"
+  }
+
+
+}
+
+mas.addEventListener('click', curtain)
+
+
+
+const pseudoLine = document.querySelector(".pseudoLine")
+console.log("lines line lines", pseudoLine)
+
+
+window.addEventListener('scroll', (e)=>{ 
+
+  const scrTop = document.scrollingElement.scrollTop
+  const scrHeight = document.scrollingElement.scrollHeight
+
+  // const pagePercent= ((scrTop+window.innerHeight)/scrHeight);
+  const pagePercent= (scrTop/scrHeight);
+
+  pseudoLine.style.width = pagePercent*document.scrollingElement.scrollWidth
+
+  console.log("top", scrTop, "hiet", scrHeight, "PERCETNAGE", Math.floor(pagePercent))
+
+// console.log("top plus window height",scrTop+window.innerHeight)
+  
+
+  console.log("window height", window.innerHeight)
+
+})
+
+console.log("scroll doc", document.scrollingElement)
